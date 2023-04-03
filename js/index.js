dd838b12e68dbfb003a9059257c5fe119755da8d
scary <zef1rflis@yandex.ru>
2023-04-03 23:14:43 +0500

    gitignore + normalize + profile buttons

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-
-const PORT = process.env.PORT || 8080
-
-const app = express()
-
-app.listen(PORT, () => console.log('server started on post ${PORT}'))
--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
 function openSearch() {
     var x = document.getElementById('search__box');
+    var y = document.getElementById('search__js');
 
     if (x.style.display == 'none'){
         x.style.display = 'block';
+        y.style.color = '#000';
+        y.style.backgroundColor = '#fff';
+        y.style.textDecorationLine = 'underline';
+    }
+    else{
+        x.style.display = 'none';
+        y.style.color = '#fff';
+        y.style.backgroundColor = '#000';
+        y.style.textDecorationLine = 'none';
+    }
+}
+
+function openNotifications() {
+    var x = document.getElementById('notifications');
+
+    if (x.style.display == 'none'){
+        x.style.display = 'flex';
     }
     else{
         x.style.display = 'none';
     }
 }
 
-function openNotifications() {
-    var x = document.getElementById('notifications');
+function openProfile() {
+    var x = document.getElementById('login__window');
 
     if (x.style.display == 'none'){
         x.style.display = 'flex';
