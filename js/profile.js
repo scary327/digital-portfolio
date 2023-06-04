7ff5ffd7b6360fc0f9625bf04cac30216519b67a
scary <zef1rflis@yandex.ru>
2023-06-04 22:22:34 +0500

    submits, btn avatar and cover, portfolio indexpage

--- js/profile.js
+++ js/profile.js
@@ -1,19 +1,3 @@
-function SignOut(){
-    $.ajax({
-        url: 'singOut.php', //перенаправить пользователя на эту страницу
-            method: 'POST',
-
-            success: function(response) {
-                //обработка успешного ответа
-                window.location.href = 'login.php' //перенаправление на страницу входа
-            },
-            error: function(xhr, status, error) {
-                //обработка ответ об ошибке
-                console.log(error);
-            }
-    });
-}
-
 function show_hide_password(target){
 	var input = document.getElementById('password__input');
 	if (input.getAttribute('type') == 'password') {
@@ -24,27 +8,24 @@ function show_hide_password(target){
 	return false;
 }
 
-function sendProjectData(){
-    const projectData = {
-        name: document.getElementById('portfolio__input__name'),
-        file: document.getElementById('document.getElementById'),
-        description: document.getElementById('portfolio__link'),
-        avatar: document.getElementById('portfolio__logo')
+function changeAvatar(){
+    var x = document.getElementById('window__avatar__input');
+
+    if (x.style.display == 'none'){
+        x.style.display = 'flex';
     }
+    else{
+        x.style.display = 'none';
+    }
+}
 
-    const jsonData = JSON.stringify(projectData);
+function changeCover(){
+    var x = document.getElementById('window__cover__input');
 
-    $.ajax({
-        url: "", //сюда вписать путь, куда эти данные отправить
-        type: "POST",
-        data: jsonData,
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function(response) {
-            console.log("отправлено на бэк");
-        },
-        error: function(error) {
-            console.error("ошибка при отправке на бэк", error);
-        }
-    });
+    if (x.style.display == 'none'){
+        x.style.display = 'flex';
+    }
+    else{
+        x.style.display = 'none';
+    }
 }
